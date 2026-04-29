c7d6e21c125d2c310fade7d493c37602e915b422
Tim Klein <klein_tim@t-online.de>
2026-04-29 23:01:55 +0200

    Anpassung der Kommentare.

--- python/unterricht/2D-Listen/Aufgabe_2/Lager.py
+++ python/unterricht/2D-Listen/Aufgabe_2/Lager.py
@@ -1,28 +1,24 @@
 import Funktionsliste as FL 
 
-# Eine Liste in lager anlegen. 
+
 lager = [
         [12, 4, 7, 0], #23
         [3, 15, 0, 9], #27
-        [0, 2, 1, 8], #11
-        ]          
+        [0, 2, 1, 8]] #11          
 
-
-
-#1. Gesamtzahl aller Produkte (61)
+#1. Gesamtzahl aller Produkte 
 print("\n--- -1.- ---")
-FL.produktanzahl_zaehlen(lager)
+FL.produktanzahl_zaehlen(lager) # Das ganze Lager umfasst aktuell 61 Produkte.
 
- #2. Regal mit den meisten Produkten (2, 27 Produkte)
+ #2. Regal mit den meisten Produkten 
 print("\n--- -2.- ---")
-FL.regal_meiste_produkte(lager)
-
+FL.regal_meiste_produkte(lager) # Reihe: 2 mit 27 Produkten.
 
 #3. Liste die alle werte unter 5 auf 0 setzt 
 print("\n--- -3.- ---")
-print(f"Alte Lagerstruktur: {lager}")
-FL.werte_zurück_setzen(lager)
+print(f"Alte Lagerstruktur: {lager}") # Alte Lagerstruktur: [[12, 4, 7, 0], [3, 15, 0, 9], [0, 2, 1, 8]]
+FL.werte_zurück_setzen(lager) # Neue Lagerstruktur: [[12, 0, 7, 0], [0, 15, 0, 9], [0, 0, 0, 8]
 
-# #4. Positionen mit leeren Regalen
+#4. Positionen mit leeren Regalen (Reihe, Regal)
 print("\n--- -4.- ---")
-FL.leere_regale_zaehlen(lager)
+FL.leere_regale_zaehlen(lager) # Leere Positionen im Lager: [(0, 3), (1, 2), (2, 0)]
---
+++ python/unterricht/2D-Listen/Aufgabe_3/Punktevergabe.py
@@ -0,0 +1,21 @@
+import Funktionsliste as FL
+
+punkte =[
+        [8, 7, 9, 6]
+        [5, 6, 5, 4]
+        [9, 9, 10, 8]
+        [3, 4, 2, 5]
+        ]
+
+
+# #1. Gesamtzahl aller Produkte (61)
+# print("\n--- -1.- ---")
+
+# #1. Gesamtzahl aller Produkte (61)
+# print("\n--- -2.- ---")
+
+# #1. Gesamtzahl aller Produkte (61)
+# print("\n--- -3.- ---")
+
+# #1. Gesamtzahl aller Produkte (61)
+# print("\n--- -4.- ---")
